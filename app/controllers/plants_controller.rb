c10d0f536e8f46159aa1071bdc8fc91351125d58
Mark Francis <markxf@gmail.com>
2019-08-26 16:14:39 +0100

    implemented only the most basic CRUD actions in the plant controller

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,3 +1,50 @@
 class PlantsController < ApplicationController
   mount_uploader :photo, PhotoUploader
+
+  before_action :set_plant, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @plants = Plant.all
+  end
+
+  def show
+  end
+
+  def new
+    @plant = Plant.new
+  end
+
+  def create
+    @plant = Plant.new(plant_params)
+    if @plant.save
+      redirect_to plant_path(@plant)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @plant.update(plant_params)
+      redirect_to plant_path(@plant)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @plant.destroy
+  end
+
+  private
+
+  def set_plant
+    @plant = Plant.find(params[:id])
+  end
+
+  def plant_params
+    params.require(:plant).permit(:name, :photo, :last_watered)
+  end
 end
