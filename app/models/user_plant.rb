625c303be02884e11e607ef3b08cffa51f7ff179
Alan Quinn <alancquinn2@gmail.com>
2019-08-28 19:28:09 +0100

    add style to modal and cards - fix buggy css class naming across app

--- app/models/user_plant.rb
+++ app/models/user_plant.rb
@@ -8,7 +8,11 @@ class UserPlant < ApplicationRecord
   end
 
   def days_until_water
-    @days = (Date.today - last_watered).to_i
+    days_since_water
+    return (self.plant.water_freq - @days)
   end
 
+  def days_since_water
+    @days = (Date.today - last_watered).to_i
+  end
 end
