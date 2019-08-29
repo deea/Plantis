40a0e3784aa5f0c1ea07b380df40f50e2e6a2c56
Stanislas Bussiere <stanislas.bussiere@gmail.com>
2019-08-29 14:10:43 +0100

    level method

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,24 @@ class User < ApplicationRecord
   mount_uploader :photo, PhotoUploader
   has_many :user_plants
 
-  # def addseeds(number_of_seeds)
-  #   user.seed_balance += number_of_seeds
-  #   user,seed_balance.save
-  #       if number_of_seeds
+
+  # def user_level(seeds)
+  #   if seeds < 100
+  #     user_level = 0
+  #   elsif seeds < 300
+  #     # user_level = 1
+  #     self.lvl = 1
+  #   elsif seeds < 600
+  #     user_level = 2
+  #   elsif seeds < 1000
+  #     user_level = 3
+  #   elsif seeds < 1500
+  #     user_level = 4
+  #   else
+  #     user_level = 5
+  #   end
+  #   self.save
   # end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_27_100208) do
+ActiveRecord::Schema.define(version: 2019_08_29_110824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2019_08_27_100208) do
     t.string "firstname"
     t.string "lastname"
     t.integer "seeds"
+    t.integer "level", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
