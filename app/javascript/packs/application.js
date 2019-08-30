f31d67cec402d9d0019f7383c001e13ca6d3990e
Alan Quinn <alancquinn2@gmail.com>
2019-08-30 14:29:27 +0100

    remove sweetalert2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,9 @@
 import "bootstrap";
 import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css"
+import "flatpickr/dist/flatpickr.min.css";
 
 flatpickr("#user_plant_last_watered", {
   dateFormat: "d/m/Y",
   maxDate: "today"
-})
+});
 
