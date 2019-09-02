2f9cb40d1e3e356e52e67214e7340b340ef6571d
Alan Quinn <alancquinn2@gmail.com>
2019-09-02 09:56:25 +0100

    redirect user to jungle after sign-in

--- db/schema.rb
+++ db/schema.rb
@@ -54,8 +54,8 @@ ActiveRecord::Schema.define(version: 2019_08_29_113421) do
     t.string "photo"
     t.string "firstname"
     t.string "lastname"
-    t.integer "level", default: 0
     t.integer "seeds", default: 0
+    t.integer "level", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
